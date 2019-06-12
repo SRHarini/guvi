6fcaca4f03d05e1d225d3f030349bca98d203726
SRHarini <51670940+SRHarini@users.noreply.github.com>
2019-06-12 12:48:45 +0530

    Update great.py

--- CodeKata/great.py
+++ CodeKata/great.py
@@ -1,7 +1,7 @@
 i,j,k=map(int,input().split())
-if(i>j and i>k):
+if(i>=j and i>=k):
   print(i)
-elif(j>i and j>k):
+elif(j>=i and j>=k):
   print(j)
-elif(k>i and k>j):
+elif(k>=i and k>=j):
   print(k)
