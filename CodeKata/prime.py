801cddab8a037c84d9edde552e0d28e8e11b5c79
SRHarini <51670940+SRHarini@users.noreply.github.com>
2019-06-12 23:17:52 +0530

    Update prime.py

--- CodeKata/prime.py
+++ CodeKata/prime.py
@@ -1,17 +1,10 @@
-g=int(input())
-if(g==2 or g==3):
-  print("yes")
-if(g>3):
-  for i in range(2,10):
-    if(g%i==0):
-      c=0
-      break
-    else:
-      c=1   
-  if(c==0):
-    print("no")
-  else:
-    print("yes")
-  
-
-
+get = int(input())  
+if (get > 1):  
+   for i in range(2,get):  
+       if (get % i) == 0:  
+           print("no")  
+           break
+   else:  
+       print("yes")  
+else:  
+   print("no")
